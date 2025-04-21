5d2a2cc6609fd311e22739f9db9ad2eca21ec6b8
thomaslaurenson <thomas@thomaslaurenson.com>
2025-04-22 10:23:02 +1200

    Reordered main subcommands and handlers

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char **argv) {
     CLI::App app{"A command line tool to read, extract, search, create and verify MPQ file using the StormLib library"};
 
     std::string target = "default";
+    std::string file = "default";
     std::string output = "default";
     bool keepFolderStructure = false;
     bool patchExtractBin = false;
@@ -35,6 +36,41 @@ int main(int argc, char **argv) {
         ->required()
         ->check(CLI::ExistingFile);
 
+    // Subcommand: Create
+    CLI::App *create = app.add_subcommand("create", "Create MPQ file from target directory");
+    create->add_option("target", target, "Target directory")
+        ->required()
+        ->check(CLI::ExistingDirectory);
+    create->add_option("-o,--output", output, "Output file");
+    create->add_option("-b,--base", baseFolder, "Base folder to use for file names");
+    create->add_option("-v,--version", mpqVersion, "MPQ version (default 1)")
+        ->check(CLI::Range(1, 2));
+
+    // Subcommand: Add
+    CLI::App *add = app.add_subcommand("add", "Add file to an existing MPQ file");
+    add->add_option("file", file, "File to add")
+        ->required()
+        ->check(CLI::ExistingFile);
+    add->add_option("target", target, "Target MPQ file")
+        ->required()
+        ->check(CLI::ExistingFile);
+
+    // Subcommand: Remove
+    CLI::App *remove = app.add_subcommand("remove", "Remove file from an existing MPQ file");
+    remove->add_option("file", target, "File to remove")
+        ->required();
+    remove->add_option("target", target, "Target MPQ file")
+        ->required()
+        ->check(CLI::ExistingFile);
+
+    // Subcommand: List
+    CLI::App *list = app.add_subcommand("list", "List files from the MPQ file");
+    list->add_option("target", target, "Target MPQ file")
+        ->required()
+        ->check(CLI::ExistingFile);
+    list->add_option("-l,--listfile", listfileName, "File listing content of MPQ")
+        ->check(CLI::ExistingFile);
+
     // Subcommand: Extract
     CLI::App *extract = app.add_subcommand("extract", "Extract files from the MPQ file");
     extract->add_option("target", target, "Target MPQ file")
@@ -46,37 +82,18 @@ int main(int argc, char **argv) {
     extract->add_option("-l,--listfile", listfileName, "File listing content of MPQ")
         ->check(CLI::ExistingFile);
 
-
-    // Subcommand: Create
-    CLI::App *create = app.add_subcommand("create", "Create MPQ file from target directory");
-    create->add_option("target", target, "Target directory")
-        ->required()
-        ->check(CLI::ExistingDirectory);
-    create->add_option("-o,--output", output, "Output file");
-    create->add_option("-b,--base", baseFolder, "Base folder to use for file names");
-    create->add_option("-v,--version", mpqVersion, "MPQ version (default 1)")
-        ->check(CLI::Range(1, 2));
-
-    // Subcommand: List
-    CLI::App *list = app.add_subcommand("list", "List files from the MPQ file");
-    list->add_option("target", target, "Target MPQ file")
-        ->required()
-        ->check(CLI::ExistingFile);
-    list->add_option("-l,--listfile", listfileName, "File listing content of MPQ")
-        ->check(CLI::ExistingFile);
-
-    // Subcommand: Verify
-    CLI::App *verify = app.add_subcommand("verify", "Verify the MPQ file");
-    verify->add_option("target", target, "Target MPQ file")
-        ->required()
-        ->check(CLI::ExistingFile);
-
     // Subcommand: Patch
     CLI::App *patch = app.add_subcommand("patch", "Various MPQ patch helpers");
     patch->add_option("target", target, "Target file")
         ->required()
         ->check(CLI::ExistingFile);
     patch->add_flag("-b,--bin", patchExtractBin, "Extract BIN file from EXE patch (default false)");
+
+    // Subcommand: Verify
+    CLI::App *verify = app.add_subcommand("verify", "Verify the MPQ file");
+    verify->add_option("target", target, "Target MPQ file")
+        ->required()
+        ->check(CLI::ExistingFile);
 
     CLI11_PARSE(app, argc, argv);
 
@@ -102,27 +119,6 @@ int main(int argc, char **argv) {
         HANDLE hArchive;
         OpenMpqArchive(target, &hArchive);
         PrintMpqInfo(hArchive);
-    }
-
-    // Handle subcommand: Extract
-    if (app.got_subcommand(extract)) {
-        // If no output directory specified, use MPQ path without extension
-        // If output directory specified, create it if it doesn't exist
-        if (output == "default") {
-            fs::path outputPathAbsolute = fs::canonical(target);
-            fs::path outputPath = outputPathAbsolute.parent_path() / outputPathAbsolute.stem();
-            std::string outputString{outputPath.u8string()};
-            output = outputString;
-        }
-        fs::create_directory(output);
-
-        HANDLE hArchive;
-        OpenMpqArchive(target, &hArchive);
-        if (extractFileName != "default") {
-            ExtractFile(hArchive, output, extractFileName, keepFolderStructure);
-        } else {
-            ExtractFiles(hArchive, output, listfileName);
-        }
     }
 
     // Handle subcommand: Create
@@ -153,6 +149,51 @@ int main(int argc, char **argv) {
         }
     }
 
+    // Handle subcommand: Add
+    if (app.got_subcommand(add)) {
+        std::cout << "[!] Not yet implemented..." << std::endl;
+    }
+
+    // Handle subcommand: Remove
+    if (app.got_subcommand(remove)) {
+        std::cout << "[!] Not yet implemented..." << std::endl;
+    }
+
+    // Handle subcommand: Extract
+    if (app.got_subcommand(extract)) {
+        // If no output directory specified, use MPQ path without extension
+        // If output directory specified, create it if it doesn't exist
+        if (output == "default") {
+            fs::path outputPathAbsolute = fs::canonical(target);
+            fs::path outputPath = outputPathAbsolute.parent_path() / outputPathAbsolute.stem();
+            std::string outputString{outputPath.u8string()};
+            output = outputString;
+        }
+        fs::create_directory(output);
+
+        HANDLE hArchive;
+        OpenMpqArchive(target, &hArchive);
+        if (extractFileName != "default") {
+            ExtractFile(hArchive, output, extractFileName, keepFolderStructure);
+        } else {
+            ExtractFiles(hArchive, output, listfileName);
+        }
+    }
+
+    // Handle subcommand: List
+    if (app.got_subcommand(list)) {
+        HANDLE hArchive;
+        OpenMpqArchive(target, &hArchive);
+        ListFiles(hArchive, listfileName);
+    }
+
+    // Handle subcommand: Patch
+    if (app.got_subcommand(patch)) {
+        HANDLE hArchive;
+        OpenMpqArchive(target, &hArchive);
+        ExtractMpqAndBinFromExe(hArchive, patchExtractBin);
+    }
+
     // Handle subcommand: Verify
     if (app.got_subcommand(verify)) {
         HANDLE hArchive;
@@ -175,20 +216,6 @@ int main(int argc, char **argv) {
         std::cout << "[+] Signature type: " << signatureType << signatureNameFormatted << std::endl;
 
         PrintMpqSignature(hArchive, signatureType);
-    }
-
-    // Handle subcommand: List
-    if (app.got_subcommand(list)) {
-        HANDLE hArchive;
-        OpenMpqArchive(target, &hArchive);
-        ListFiles(hArchive, listfileName);
-    }
-
-    // Handle subcommand: Patch
-    if (app.got_subcommand(patch)) {
-        HANDLE hArchive;
-        OpenMpqArchive(target, &hArchive);
-        ExtractMpqAndBinFromExe(hArchive, patchExtractBin);
     }
 
     return 0;
