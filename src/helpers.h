d97bacdce65dd9594f7e0a3a95d512292ea26453
thomaslaurenson <thomas@thomaslaurenson.com>
2025-08-11 03:38:56 +1200

    Updated max files helpers for archive creation

--- src/helpers.h
+++ src/helpers.h
@@ -10,7 +10,7 @@ std::string FileTimeToLsTime(int64_t fileTime);
 std::string LocaleToLang(uint16_t locale);
 std::string NormalizeFilePath(const fs::path &path);
 std::string WindowsifyFilePath(const fs::path &path);
-int CountFilesInDirectory(const std::string &directory);
+int32_t CalculateMpqMaxFileValue(const std::string &directory);
 bool IsPrintable(const char* buffer, uint32_t size);
 void PrintAsText(const char* buffer, uint32_t size);
 void PrintAsHex(const char* buffer, uint32_t size);
