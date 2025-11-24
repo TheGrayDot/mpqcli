f4e3c086463a48a693bd1b4f601891e57fabfcf5
Thomas Laurenson <thomas@thomaslaurenson.com>
2025-11-24 19:45:42 +1300

    Remove un-needed, duplicate stdout line

--- src/mpq.cpp
+++ src/mpq.cpp
@@ -173,8 +173,6 @@ int AddFiles(HANDLE hArchive, const std::string& target, LCID locale) {
 }
 
 int AddFile(HANDLE hArchive, const fs::path& localFile, const std::string& archiveFilePath, LCID locale) {
-    std::cout << "[+] Adding file: " << archiveFilePath << std::endl;
-
     // Return if file doesn't exist on disk
     if (!fs::exists(localFile)) {
         std::cerr << "[!] File doesn't exist on disk: " << localFile << std::endl;
