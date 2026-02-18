d5e43e328eba76251c1f81333b1df4a86e75ff65
Johan Sjöblom <sjoblom.johan@gmail.com>
2026-02-19 08:01:37 +1300

    Support unfamiliar locales (#136)
    - List unfamiliar locales
    - Better printing of locales
    - Locale fixes

--- src/locales.h
+++ src/locales.h
@@ -11,14 +11,21 @@ const LCID defaultLocale = 0;
 
 std::string LocaleToLang(uint16_t locale);
 LCID LangToLocale(const std::string &lang);
+LCID ParseHexLocale(const std::string& str);
 std::vector<std::string> GetAllLocales();
+std::string PrettyPrintLocale(LCID locale, const std::string &prefix = "", bool alwaysPrint = false);
 
 // Validator for CLI11
 const inline auto LocaleValid = CLI::Validator(
         [](const std::string &str) {
             if (str == "default") return std::string();
 
-            LCID locale = LangToLocale(str);
+            // Check if it's a 4-character hexadecimal string
+            if (ParseHexLocale(str) != defaultLocale) {
+                return std::string();
+            }
+
+            const LCID locale = LangToLocale(str);
             if (locale == 0) {
                 std::string validLocales = "Locale must be nothing, or one of:";
                 for (const auto& l : GetAllLocales()) {
@@ -28,7 +35,7 @@ const inline auto LocaleValid = CLI::Validator(
             }
             return std::string();
         },
-        "Validates locales and outputs valid locales",
+        "",
         "LocaleValidator"
 );
 
